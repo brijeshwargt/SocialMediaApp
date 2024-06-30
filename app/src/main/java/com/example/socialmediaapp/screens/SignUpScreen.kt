ca0d9bb65544acd394fa10bde3c227ac9240df5d
brijeshwargt <brijeshwar3012gt@gmail.com>
2024-07-01 03:35:31 +0530

    added image input in sign up screen

--- app/src/main/java/com/example/socialmediaapp/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/socialmediaapp/screens/SignUpScreen.kt
@@ -1,6 +1,13 @@
 package com.example.socialmediaapp.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -12,9 +19,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Email
@@ -33,7 +43,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -42,7 +56,10 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
+import com.example.socialmediaapp.R
 import com.example.socialmediaapp.navigation.Routes
 
 @Composable
@@ -53,16 +70,44 @@ fun SignUpScreen(navHostController: NavHostController) {
     var bio by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+
     var isErrorInEmail by remember { mutableStateOf(false) }
     var isErrorInPassword by remember { mutableStateOf(false) }
     var isPasswordVisible by remember { mutableStateOf(false) }
-
     val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    val permissionToRequest = if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU) {
+        Manifest.permission.READ_MEDIA_IMAGES
+    }
+    else {
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    val context = LocalContext.current
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+
+        } else {
+
+        }
+    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .background(color = if (!isSystemInDarkTheme()) Color.LightGray else Color.Transparent)
     ) {
         Text(
@@ -72,7 +117,41 @@ fun SignUpScreen(navHostController: NavHostController) {
             letterSpacing = 2.sp,
 //            color = MaterialTheme.colorScheme.primary
         )
-        Spacer(modifier = Modifier.height(100.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+        Image(
+            painter = if(imageUri == null) painterResource(id = R.drawable.woman)
+            else rememberAsyncImagePainter(model = imageUri),
+            contentDescription = "person",
+            contentScale = ContentScale.Crop,
+            alpha = 0.9f,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+                val isGranted = ContextCompat.checkSelfPermission(
+                    context, permissionToRequest
+                ) == PackageManager.PERMISSION_GRANTED
+
+                if (isGranted) {
+                    launcher.launch("image/*")
+                } else {
+                    permissionLauncher.launch(permissionToRequest)
+                }
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "add icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(text = "Add Photo")
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+//        Spacer(modifier = Modifier.height(100.dp))
         OutlinedTextField(
             value = name,
             onValueChange = {
@@ -108,7 +187,7 @@ fun SignUpScreen(navHostController: NavHostController) {
             singleLine = true,
         )
         OutlinedTextField(
-            value = bio ,
+            value = bio,
             onValueChange = {
                 bio = it
             },
@@ -180,14 +259,14 @@ fun SignUpScreen(navHostController: NavHostController) {
                 keyboardType = KeyboardType.Password
             ),
             singleLine = true,
-            visualTransformation = if(isPasswordVisible) VisualTransformation.None
-                else PasswordVisualTransformation(),
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None
+            else PasswordVisualTransformation(),
             trailingIcon = {
                 val image = if (isPasswordVisible) Icons.Default.Lock else null
 
             }
         )
-        Spacer(modifier = Modifier.height(50.dp))
+        Spacer(modifier = Modifier.height(30.dp))
         OutlinedButton(
             onClick = { /*TODO*/ },
             border = BorderStroke(.5.dp, MaterialTheme.colorScheme.onBackground),
